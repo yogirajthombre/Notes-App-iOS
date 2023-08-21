9951c08e630ebeb0a4de41cac4dab4907ef84aba
yogirajthombre93@gmail.com <yogirajthombre93@gmail.com>
2023-08-21 12:46:29 +0530

    Implemented coreData base class for loading the model

---
+++ NotesApp/CoreData/CoreDataStack.swift
@@ -0,0 +1,40 @@
+//
+//  CoreDataStack.swift
+//  NotesApp
+//
+//  Created by Yogiraj Thombre on 8/21/23.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    
+    private let modelName : String
+    
+    init(modelName : String){
+        self.modelName = modelName
+    }
+    
+    private lazy var storeContainer : NSPersistentContainer = {
+        let container = NSPersistentContainer(name: self.modelName)
+        container.loadPersistentStores { success , error in
+            if let error = error as NSError? {
+                print("Un Resolved Error ...")
+            }
+        }
+        return container
+    }()
+    
+    lazy var managedContext : NSManagedObjectContext = self.storeContainer.viewContext
+    
+    func saveContext(){
+        guard managedContext.hasChanges else {return}
+        do {
+            try managedContext.save()
+        }catch let _ as NSError {
+            print("Error in saving the object .....")
+        }
+    }
+    
+}
