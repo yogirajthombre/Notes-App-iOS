3bd17178c7d4a794f598be2009535aa0eb622a71
yogirajthombre93@gmail.com <yogirajthombre93@gmail.com>
2023-09-04 18:09:00 +0530

    Improved UI of the EditNotesController screen

--- NotesApp/EditNotesController.swift
+++ NotesApp/EditNotesController.swift
@@ -6,78 +6,63 @@
 //
 
 import UIKit
+import KMPlaceholderTextView
 
 class EditNotesController: UIViewController {
     
     var notesModel = [Note]()
     
-    var notesTitle : UITextField = {
-            let title = UITextField()
+    var notesTitle : KMPlaceholderTextView = {
+            let title = KMPlaceholderTextView()
             title.translatesAutoresizingMaskIntoConstraints = false
-            title.borderStyle = .roundedRect
-            title.font = UIFont(name: "GillSans", size: 24)
-            title.backgroundColor = .white
-            title.attributedPlaceholder = NSAttributedString(string: "Add Notes Title Here ....", attributes: [NSAttributedString.Key.foregroundColor:UIColor.black])
+            title.font = UIFont(name: "GillSans", size: 34)
+            title.layer.cornerRadius = 10
+            title.layer.borderWidth = 1.0
+            title.layer.borderColor = UIColor.lightGray.cgColor
+            title.placeholder = "Title . . ."
+            title.clipsToBounds = true
             title.textColor = .black
+            title.backgroundColor = .white
+            title.isScrollEnabled = false
+            title.textContainerInset = UIEdgeInsets(top: 20, left: 0, bottom: 20, right: 0)
             return title
     }()
     
-    var notesContent : UITextField = {
-            let title = UITextField()
-            title.translatesAutoresizingMaskIntoConstraints = false
-            title.borderStyle = .roundedRect
-            title.font = UIFont(name: "GillSans", size: 24)
-            title.textAlignment = .left
-            title.contentVerticalAlignment = .top
-            title.backgroundColor = .white
-            title.attributedPlaceholder = NSAttributedString(string: "Add Content Here ....", attributes: [NSAttributedString.Key.foregroundColor:UIColor.black])
-            title.textColor = .black
-            return title
-    }()
-    
-    var saveButton : UIButton = {
-            let button = UIButton(type: .roundedRect)
-            button.translatesAutoresizingMaskIntoConstraints = false
-            button.setTitle("Save", for: .normal)
-            button.layer.cornerRadius = 10
-            button.layer.shadowRadius = 10
-            button.titleLabel?.textColor = UIColor.white
-            button.titleLabel?.font = UIFont(name: "GillSans", size: 24)
-            button.backgroundColor = .black
-            button.addTarget(self, action: #selector(saveNotes), for: .touchUpInside)
-            return button
+    var notesContent : KMPlaceholderTextView = {
+        let content = KMPlaceholderTextView()
+        content.translatesAutoresizingMaskIntoConstraints = false
+        content.font = UIFont(name: "GillSans", size: 26)
+        content.layer.cornerRadius = 10
+        content.clipsToBounds = true
+        content.textColor = .black
+        content.placeholder = "Note"
+        content.backgroundColor = .white
+        content.isScrollEnabled = false
+        return content
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.backgroundColor = .blue
+        view.backgroundColor = .white
         addSubView()
         constraintsInit()
     }
     
     func constraintsInit(){
         
-        notesTitle.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 30).isActive = true
-        notesTitle.trailingAnchor.constraint(equalTo: self.view.trailingAnchor,constant: -30).isActive = true
-        notesTitle.topAnchor.constraint(equalTo: view.topAnchor,constant: 120).isActive = true
-        notesTitle.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        notesTitle.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 20).isActive = true
+        notesTitle.trailingAnchor.constraint(equalTo: self.view.trailingAnchor,constant: -20).isActive = true
+        notesTitle.topAnchor.constraint(equalTo: view.topAnchor,constant: 50).isActive = true
         
-        notesContent.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 30).isActive = true
-        notesContent.trailingAnchor.constraint(equalTo: self.view.trailingAnchor,constant: -30).isActive = true
+        notesContent.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 20).isActive = true
+        notesContent.trailingAnchor.constraint(equalTo: self.view.trailingAnchor,constant: -20).isActive = true
         notesContent.topAnchor.constraint(equalTo: notesTitle.bottomAnchor,constant: 30).isActive = true
-        notesContent.heightAnchor.constraint(equalToConstant: 380).isActive = true
-        
-        saveButton.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 100).isActive = true
-        saveButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor,constant: -100).isActive = true
-        saveButton.topAnchor.constraint(equalTo: notesContent.bottomAnchor,constant: 30).isActive = true
-        saveButton.heightAnchor.constraint(equalToConstant: 60).isActive = true
     }
     
     func addSubView(){
         view.addSubview(notesTitle)
         view.addSubview(notesContent)
-        view.addSubview(saveButton)
     }
     
     @objc func saveNotes(_sender : UIButton?){
